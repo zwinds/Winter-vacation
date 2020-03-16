e7c7aca12a7d9226a5c80aeb84a078ff92184e60
zwinds <jody1225@qq.com>
2020-03-16 14:08:06 +0800

    yarn的使用

---
+++ Node.js/11yarn.js
@@ -0,0 +1,33 @@
+/*
+    yarn工具基本使用
+
+    安装yarn工具：npm install -g yarn
+
+    1、初始化包
+    npm init
+    yarn init
+    2、安装包
+    npm install xxx --save
+    yarn add xxx
+    3、移除包
+    npm uninstall xxx
+    yarn remove xxx
+    4、更新包
+    npm update xxx
+    yarn upgrade xxx
+    5、安装开发依赖的包
+    npm install xxx --save-dev
+    yarn add xxx --dev
+    6、全局安装
+    npm install -g xxx
+    yarn global add xxx
+    7、设置下载镜像的地址
+    npm config set registry url
+    yarn config set registry url
+    8、安装所有依赖
+    npm install
+    yarn install
+    9、执行包
+    npm run
+    yarn run
+*/
